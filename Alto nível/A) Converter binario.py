105979abffeeb1b8634d5f25d8c8d292ceb9c706
Vitor da Fonseca Rodrigues <vitorfon20@gmail.com>
2023-06-20 19:48:46 -0300

    Update A) Converter binario.py

--- Alto nível/A) Converter binario.py
+++ Alto nível/A) Converter binario.py
@@ -1,9 +1,8 @@
-numero = int(input("Digite um número inteiro: "))
-binario = bin(numero)[2:]
-print(binario)
-vetor_binario = []
+def numero_para_vetor_binario(numero, tamanho):
+    binario = format(numero, '0' + str(tamanho) + 'b')  # Obtém a representação binária com zeros à esquerda
+    vetor_binario = [int(digito) for digito in binario]  # Converte cada dígito em um inteiro e cria uma lista
+    return vetor_binario
 
-for digito in binario:
-    vetor_binario.append(digito)
-
-print("Vetor binário:", vetor_binario)
+n = int(input("Digite o valor de n: "))
+ReprN = numero_para_vetor_binario(n,32)
+print("Vetor binário:", ReprN)
